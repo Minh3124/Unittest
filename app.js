99f5294d48ffb488ddde4bd34041375a35738b9a
DESKTOP-1B2N9JN\caoth <caothibinhminh@gmail.com>
2020-02-07 12:05:43 +0100

    no message

--- app.js
+++ app.js
@@ -9,17 +9,29 @@ module.exports = { ageValidation, heightValidation, weightValidation, computeBMI
 
 
 rl.question('Welcome to BMI calculator. How old are you?(0-150)', (answer) => {
-  var x=ageValidation(answer)
-  var weight,height
-  while(x==false){
+  var x=ageValidation(answer),w,h;
+  var weight,height;
+  if (x==true){
+    rl.question('How tall are you?', (answer1) => {
+      height=answer1;
+      h=heightValidation(height);
+      if (h==true){
+        rl.question('How about your weight?', (answer2) => {
+          weight=answer2;
+          w= weightValidation(weight);
+          if(w==true){
+            bmi=computeBMI(height,weight);
+            BMImeaning(bmi);
+          }   
+          }); 
+    }
+      
+}); 
+   
+  } else {
     rl.question('How old are you', (answer) => {
         x= answer
     }); 
-  }
-  if (x==true){
-    heightValidation(height);
-    weightValidation(weight);
-    computeBMI(height,weight);
   }
 
 
@@ -29,6 +41,9 @@ function ageValidation(age){
   
   if (isNaN(age) || age < 0 || age > 150) {
     console.log("Please enter your real age!") ;
+    rl.question('Please answer again?', (answer) => {
+      x=ageValidation(answer);
+    }); 
     return false;
   } else {
     console.log("Thank you for honest answer");
@@ -38,11 +53,13 @@ function ageValidation(age){
 };
 
 function heightValidation(height){
-  rl.question('How tall are you?', (answer) => {
-  height=answer
-  }); 
+
     if (isNaN(height) || height < 10 || height > 300) {
       console.log("Are you a giant or ?") ;
+      rl.question('Please answer again?', (answer1) => {
+        height=answer1;
+        h=heightValidation(height);
+      });
       return false;
     } else {
       return true;
@@ -50,11 +67,12 @@ function heightValidation(height){
 
 };
 function weightValidation(weight){
-  rl.question('How about your weight?', (answer) => {
-    weight=answer
-    }); 
     if (isNaN(weight) || weight < 0 || weight > 700) {
       console.log("Are you a giant or ?") ;
+      rl.question('Please answer again?', (answer2) => {
+        weight=answer2;
+        w=weightValidation(weight);
+      });
       return false;
     } else {
       return true;
@@ -66,21 +84,25 @@ function computeBMI(height, weight)
     height=height/100;
     BMI=weight/Math.pow(height,2);
     BMI = Math.round( BMI * 10 ) / 10;
+    console.log("Your BMI:"+BMI)
     return BMI;
 }
 
 function BMImeaning(bmi){
   if (bmi<18.5){
     console.log("Underweight");
+    return "Underweight";
   }  
-else   if (bmi>=18.5 && output<=25){
+else   if (bmi>=18.5 && bmi<=25){
     console.log("Normal");
+    return "Normal";
 }
-else   if (bmi>=25 && output<=30){
+else   if (bmi>=25 && bmi<=30){
     console.log("Overweight");
-
+    return "Overweight";
 }
-else   if (bmi>30){
+else if (bmi>30){
     console.log("Obesity");
-}
+    return "Obesity";
+};
 }
